b19c852d3343d84bcd2251e5fece2ae5064d35a5
miscbits <willow6786@gmail.com>
2018-04-06 14:13:03 -0400

    added routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,5 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('flashcards', 'FlashcardsController');
+Route::resource('sections', 'SectionsController');
